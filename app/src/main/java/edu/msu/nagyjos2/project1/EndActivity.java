f6a1e9461017c970fc845a13e1ab991bdd1d14ea
Matthew Delanoy <delanoym@cse.msu.edu>
2021-10-21 18:40:18 -0400

    Added player name strings to end activity and set the text field to player name string.

--- app/src/main/java/edu/msu/nagyjos2/project1/EndActivity.java
+++ app/src/main/java/edu/msu/nagyjos2/project1/EndActivity.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class EndActivity extends AppCompatActivity {
+
+    private String player1_name;
+    private String player2_name;
+    private TextView Winner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end);
+        player1_name = getIntent().getExtras().getString("Player1Name");
+        player2_name = getIntent().getExtras().getString("Player2Name");
+        Winner = (TextView) findViewById(R.id.winnerName);
+        Winner.setText(player1_name);
     }
 
     public void onNewGame(View view) {
