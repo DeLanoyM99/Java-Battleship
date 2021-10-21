df2f1404f10976c46d889d1be105ac159b7466f1
Zhendong Liu <liuzhend@msu.edu>
2021-10-21 18:43:50 -0400

    surrender button code

--- app/src/main/java/edu/msu/nagyjos2/project1/MainActivity.java
+++ app/src/main/java/edu/msu/nagyjos2/project1/MainActivity.java
@@ -18,4 +18,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ShipPlacement.class);
         startActivity(intent);
     }
+
+    public void onSurrenderButtom(View view) {
+        Intent intent = new Intent(this, EndActivity.class);
+        startActivity(intent);
+    }
 }
