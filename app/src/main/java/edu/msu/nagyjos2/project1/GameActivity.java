c028cbcdc04243c2d9555f79c3f8cfd7d706cd24
Matthew Delanoy <delanoym@cse.msu.edu>
2021-10-28 20:59:12 -0400

    Added different message to menu for game activity.

--- app/src/main/java/edu/msu/nagyjos2/project1/GameActivity.java
+++ app/src/main/java/edu/msu/nagyjos2/project1/GameActivity.java
@@ -1,10 +1,15 @@
 package edu.msu.nagyjos2.project1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,6 +20,35 @@ public class GameActivity extends AppCompatActivity {
     private String player1_name;
     private String player2_name;
     private TextView PlayersTurn;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_help, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_help:
+                onMenuPlacement(getGameView());
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onMenuPlacement (View view) {
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder(view.getContext());
+        builder.setTitle("Click a square to attack, if you miss a circle will appear. " +
+                        "If you hit an opponet's ship, an x will appear.");
+        builder.setPositiveButton(android.R.string.ok, null);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
 
     @SuppressLint("SetTextI18n")
     @Override
