092473fc09c2a4bbef3823e4eee4962bead665ee
ernster2 <ernster2@msu.edu>
2021-11-25 03:10:46 -0500

    Added basic xmls

---
+++ app/src/main/java/edu/msu/nagyjos2/project1/LobbyActivity.java
@@ -0,0 +1,14 @@
+package edu.msu.nagyjos2.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LobbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby);
+    }
+}
